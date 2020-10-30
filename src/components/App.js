3c371daa6d23cb8d75276eac0e1d724effad43eb
Dustin Jackson <dustin.guy.jackson@gmail.com>
2020-10-29 17:23:39 -0700

    heeey got all the config done, going to implement hot reloading on client side, then work on server side later

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,59 @@
-{
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/'
+      },
+    devServer: {
+        contentBase: "./build"
+    },
+    mode: 'development',
     module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
-      ]
-    }
-  }
+        rules: [
+            {
+                test: [/\.js$/, /\.jsx$/],
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    {
+                        // creates style nodes from JS strings
+                        loader: "style-loader",
+                    },
+                    {
+                        // translates CSS into CommonJS
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // compiles Sass to CSS
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            inject: true
+        })
+    ]
+};
